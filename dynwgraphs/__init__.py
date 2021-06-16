f5e310b98cafc458f986313a5662818beb587259
Domenico Di Gangi <digangidomenico@gmail.com>
2021-06-16 11:24:58 +0200

    feat: added auto loading of test w data at import

--- dynwgraphs/__init__.py
+++ dynwgraphs/__init__.py
@@ -5,7 +5,19 @@ __email__ = 'digangidomenico@gmail.com'
 __version__ = '0.1.0'
 
 
+
+import os
+import numpy as np
+import pkg_resources
+
 from .dirSpW1_dynNets import dirSpW1_staNet
 from .dirSpW1_dynNets import dirSpW1_dynNet_SD
 from .dirBin1_dynNets import dirBin1_staNet
 from .dirBin1_dynNets import dirBin1_dynNet_SD 
+
+
+
+# load test data
+stream =  pkg_resources.resource_stream(__name__, os.path.join("..", "tests", "test_data", "dir_w_test_data.npz"))
+
+_test_w_data_ = dict(np.load(stream))
