6ab78700032f29348e70111c834b00c78562ff60
Domenico Di Gangi <digangidomenico@gmail.com>
2021-08-24 20:52:13 +0200

    fix: second opt attempt keeps max_opt_n

--- dynwgraphs/dirGraphs1_dynNets.py
+++ dynwgraphs/dirGraphs1_dynNets.py
@@ -1329,16 +1329,12 @@ class dirGraphs_SD(dirGraphs_sequence_ss):
         try:
             return self.estimate_sd(**kwargs)
         except:
-            original_opt_n = self.opt_options_sd["opt_n"]
-            original_max_opt_iter = self.opt_options_sd["max_opt_iter"]
             secont_opt_n = "ADAM"
-            second_n_iter = original_max_opt_iter
             second_lr = self.opt_options_sd["lr"]
-            logger.warning(f"An error occurred in opt using {self.opt_options_sd['opt_n']}. Trying with  {secont_opt_n} for {second_n_iter} opt steps and lr = {second_lr}")
+            logger.warning(f"An error occurred in opt using {self.opt_options_sd['opt_n']}. Trying with  {secont_opt_n} with lr = {second_lr}")
             # if error, reset parameters and change opt algo
             self.init_all_stat_par()
             self.opt_options_sd["opt_n"] = secont_opt_n
-            self.opt_options_sd["max_opt_iter"] = second_n_iter
             self.opt_options_sd["lr"] = second_lr
  
             return self.estimate_sd(**kwargs)
