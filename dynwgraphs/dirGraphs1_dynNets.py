40d4a3e81de5ecf4ee3990637a538b0f0fc7ce18
Domenico Di Gangi <digangidomenico@gmail.com>
2021-10-07 20:14:54 +0200

    fix: fixed bugs in score_t

--- dynwgraphs/dirGraphs1_dynNets.py
+++ dynwgraphs/dirGraphs1_dynNets.py
@@ -1886,50 +1886,64 @@ class dirSpW1_sequence_ss(dirGraphs_sequence_ss):
             # TODO Add the analytical gradient for beta and dist_par_un KEEPin MIND the ovflw limit for dist_par_un  
             if dist_par_un_flag:
                 score_dict["dist_par_un"] = grad(like_t, dist_par_un_t, create_graph=True)[0]
-            if rescale_flag:
-                raise "Rescaling not ready for beta and dist par"
-                # pass
-        
+                if rescale_flag:
+                    pass
+                    # raise "Rescaling not ready for dist par"
+            
         if phi_flag | beta_flag:
             sigma_mat = self.link_dist_par(dist_par_un_t, self.N)
             log_cond_exp_Y, log_cond_exp_Y_restr, cond_exp_Y = self.exp_of_fit_plus_reg(phi_t, beta=beta_t, X_t=X_t, ret_all=True)
 
             if self.distr == 'gamma':
-                tmp = (Y_t.clone()/cond_exp_Y - A_t)*sigma_mat
+                d_logg_d_exponent = (Y_t.clone()/cond_exp_Y - A_t)*sigma_mat
                 if rescale_flag:
-                    resc_mat = sigma_mat*Y_t.clone()/cond_exp_Y 
-
+                    d2_logg_d2_exponent = - sigma_mat*Y_t.clone()/cond_exp_Y 
+                    
             elif self.distr == 'lognormal':
                 sigma2_mat = sigma_mat**2
                 log_Y_t = torch.zeros(self.N, self.N)
                 log_Y_t[A_t_bool] = Y_t[A_t_bool].log()
-                tmp = (log_Y_t - log_cond_exp_Y_restr*A_t)/sigma2_mat + A_t/2
+                d_logg_d_exponent = (log_Y_t - log_cond_exp_Y_restr*A_t)/sigma2_mat + A_t/2
                 if rescale_flag:
-                    resc_mat = (sigma_mat**2)#**Y_t.clone() 
-                    raise Exception('need to compute and update rescaling matrix ')
+                    raise Exception("need to update rescaling for log normal")
+                    d2_logg_d2_exponent = None  
+                    resc_mat = d2_logg_d2_exponent
 
             if self.avoid_ovflw_fun_flag:
                 L = self.ovflw_exp_L_limit
                 U = self.ovflw_exp_U_limit
                 # multiply the score by the derivative of the overflow limit function
                 soft_bnd_der_mat = (1 - torch.tanh(2*((log_cond_exp_Y - L)/(L - U)) + 1)**2)
-                tmp = soft_bnd_der_mat * tmp
+                d_logg_d_exponent = soft_bnd_der_mat * d_logg_d_exponent 
+                
                 if rescale_flag:
-                    resc_mat = resc_mat * (soft_bnd_der_mat**2)
+                    d2_logg_d2_exponent = d2_logg_d2_exponent * (soft_bnd_der_mat**2)
 
             if beta_flag:
-                raise "need to double check, rescale and update binary one"
-                score_dict["beta"] = (tmp * X_t).sum()
+                if self.size_beta_t > 1:
+                    raise Exception("score for multi dimensional beta not ready")
+                if self.n_reg > 1:
+                    raise Exception("need to modify beta score to handle multi")
+                score_beta = (d_logg_d_exponent * X_t[:, :, 0]).sum(dim=(0,1)) 
+
+             
+                if rescale_flag:
+                    #diagonal of the hessian wrt betas
+                    hess_diag_beta = (d2_logg_d2_exponent * X_t[:, :, 0] * X_t[:, :, 0]).sum(dim=(0,1))                
+                    resc_vec_beta = (-hess_diag_beta).sqrt()
+                    score_beta = score_beta / resc_vec_beta
+
+                score_dict["beta"] = score_beta
 
 
             if phi_flag:
-                score_phi = strIO_from_mat(tmp)
+                score_phi = strIO_from_mat(d_logg_d_exponent)
 
                 #rescale score if required
                 if rescale_flag:
-                    diag_resc = torch.diag(resc_mat)
-                    diag_resc[diag_resc==0] = 1
-                    score_phi = score_phi/diag_resc.sqrt()
+                    diag_resc_phi = -strIO_from_mat(d2_logg_d2_exponent)
+                    diag_resc_phi[diag_resc_phi==0] = 1
+                    score_phi = score_phi/diag_resc_phi.sqrt()
 
                 score_dict["phi"] = score_phi
 
@@ -2012,7 +2026,6 @@ class dirBin1_sequence_ss(dirGraphs_sequence_ss):
             raise Exception("matrix non positive")
         if not check_mat_bin.all().item():
             raise Exception("matrix non binary")
-
 
     def invPiMat(self, phi, beta, X_t, ret_log=False):
         """
@@ -2184,25 +2197,16 @@ class dirBin1_sequence_ss(dirGraphs_sequence_ss):
         A_t = Y_t 
 
         score_dict = {}
-        if  beta_flag:
-            
-            # compute the score with AD using Autograd
-            like_t = self.loglike_t(Y_t, phi_t, beta=beta_t, X_t=X_t)
-
-            score_dict["beta"] = grad(like_t, beta_t, create_graph=True)[0]
-        
-            if rescale_flag:
-                pass
-                # raise "Rescaling not ready for beta and dist par"
 
         if phi_flag:
 
             exp_A = self.exp_A(phi_t, beta=beta_t, X_t=X_t)
 
-            tmp = A_t - exp_A
+            d_logp_d_exponent = A_t - exp_A
 
             if rescale_flag:
-                resc_mat = exp_A * (1 - exp_A)
+                d2_logg_d2_exponent = - exp_A * (1 - exp_A)
+                resc_mat = - d2_logg_d2_exponent
 
             if self.avoid_ovflw_fun_flag:
                 log_inv_pi__mat = self.invPiMat(phi_t, beta=beta_t, X_t=X_t, ret_log=True)
@@ -2210,11 +2214,11 @@ class dirBin1_sequence_ss(dirGraphs_sequence_ss):
                 U = self.ovflw_exp_U_limit
                 # multiply the score by the derivative of the overflow limit function
                 soft_bnd_der_mat = (1 - torch.tanh(2*((log_inv_pi__mat - L)/(L - U)) + 1)**2)
-                tmp = soft_bnd_der_mat * tmp
+                d_logp_d_exponent = soft_bnd_der_mat * d_logp_d_exponent
                 if rescale_flag:
                     resc_mat = resc_mat * (soft_bnd_der_mat**2)
 
-            score_phi = strIO_from_mat(tmp)
+            score_phi = strIO_from_mat(d_logp_d_exponent)
      
             #rescale score if required
             if rescale_flag:
@@ -2223,6 +2227,23 @@ class dirBin1_sequence_ss(dirGraphs_sequence_ss):
                 score_phi = score_phi/diag_resc.sqrt()
 
             score_dict["phi"] = score_phi
+
+
+        if beta_flag:
+            if self.size_beta_t > 1:
+                raise Exception("score for multi dimensional beta not ready")
+            
+            if self.n_reg > 1:
+                raise Exception("need to adjust beta score for multiple regressors")
+            score_beta = (d_logp_d_exponent * X_t[:, :, 0]).sum(dim=(0,1)) 
+            if rescale_flag:
+                #diagonal of the hessian wrt betas
+                hess_diag_beta = (d2_logg_d2_exponent * X_t[:, :, 0] * X_t[:, :, 0]).sum(dim=(0,1))                
+                resc_vec_beta = (-hess_diag_beta).sqrt()
+                score_beta = score_beta / resc_vec_beta
+
+            score_dict["beta"] = score_beta
+
     
         return score_dict
 
